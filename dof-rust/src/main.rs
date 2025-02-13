13eed777d502aed48a548140affd4b61d968ee36
sophia <scastellarin@quansight.com>
2025-02-12 21:19:01 -0800

    Setup push/pull commands in cli

--- dof-rust/src/main.rs
+++ dof-rust/src/main.rs
@@ -2,6 +2,9 @@ use clap::{Parser, Subcommand};
 
 /// Simple program to greet a person
 #[derive(Parser)]
+#[command(name = "dof")]
+#[command(version = "0.0")]
+#[command(about = "checkpoint conda environments", long_about = None)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
@@ -12,15 +15,15 @@ struct Cli {
 enum Commands {
     #[command(about = "Pull a checkpoint for a remote repo", long_about = None)]
     Pull {
-        // target to push to
+        #[arg(short, long, help="namespace/environemnt:rev to pull from")]
         target: String,
     },
     #[command(about = "Push a checkpoint to a remote repo", long_about = None)]
     Push {
-        #[arg(short, long)]
+        #[arg(short, long, help="remote namespace/environemnt:rev to push to")]
         target: String,
 
-        #[arg(short, long)]
+        #[arg(short, long, help="local revision to push")]
         rev: String,
     },
 }
