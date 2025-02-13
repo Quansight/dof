d2fe784e70b303a7e49b796bdd04b180683bf42f
sophia <scastellarin@quansight.com>
2025-02-12 21:43:41 -0800

    Add checkpoint commands

--- dof-rust/src/main.rs
+++ dof-rust/src/main.rs
@@ -1,7 +1,7 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, Args};
 
 /// Simple program to greet a person
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(name = "dof")]
 #[command(version = "0.0")]
 #[command(about = "checkpoint conda environments", long_about = None)]
@@ -11,13 +11,14 @@ struct Cli {
     command: Option<Commands>,
 }
 
-#[derive(Subcommand)]
+#[derive(Debug, Subcommand)]
 enum Commands {
     #[command(about = "Pull a checkpoint for a remote repo", long_about = None)]
     Pull {
         #[arg(short, long, help="namespace/environemnt:rev to pull from")]
         target: String,
     },
+    
     #[command(about = "Push a checkpoint to a remote repo", long_about = None)]
     Push {
         #[arg(short, long, help="remote namespace/environemnt:rev to push to")]
@@ -25,6 +26,43 @@ enum Commands {
 
         #[arg(short, long, help="local revision to push")]
         rev: String,
+    },
+
+    #[command(about = "Manage checkpoints", long_about = None)]
+    Checkpoint(CheckpointArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+struct CheckpointArgs {
+    #[command(subcommand)]
+    command: Option<CheckpointCommands>,
+}
+
+#[derive(Debug, Subcommand)]
+enum CheckpointCommands {
+    #[command(about = "Delete a previous revision of the environment", long_about = None)]
+    Delete { 
+        #[arg(short, long, help="uuid of the revision to delete")]
+        rev: Option<String> 
+    },
+    
+    #[command(about = "Generate a diff of the current environment to the specified revision", long_about = None)]
+    Diff { 
+        #[arg(short, long, help="uuid of the revision to diff against")]
+        rev: Option<String> 
+    },
+    
+    #[command(about = "List all checkpoints for the current environment", long_about = None)]
+    List {},
+    
+    #[command(about = "Install a previous revision of the environment", long_about = None)]
+    Install {},
+    
+    #[command(about = "Create a lockfile for the current env and set a checkpoint", long_about = None)]
+    Save { 
+        #[arg(short, long, help="tags for the checkpoint")]
+        tag: Option<String> 
     },
 }
 
@@ -37,6 +75,9 @@ fn main() {
         }
         Some(Commands::Push { target, rev }) => {
             println!("'push' was used, target is: {:?}, rev is {:?}", target, rev)
+        }
+        Some(Commands::Checkpoint(checkpoint)) => {
+            println!("'checkpoint' was used")
         }
         None => {
             println!("idk");
