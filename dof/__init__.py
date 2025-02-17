3e83b5c7afee2492d245cd20ca3e08155fc4ece2
pdmurray <peynmurray@gmail.com>
2025-02-17 11:02:54 -0800

    Bridge to rust works!

---
+++ dof/__init__.py
@@ -0,0 +1,5 @@
+from ._dof import install_pypi
+
+
+def run():
+    install_pypi('hello', ['hoy', 'hi!'])
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,48 @@
+use pyo3::types::PyList;
+use std::path::Path;
 use pyo3::prelude::*;
+use rattler_lock::LockedPackage;
 
-/// Formats the sum of two numbers as string.
+
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn install_pypi<'py>(py: Python<'py>, prefix: String, packages: &Bound<'py, PyList>) -> PyResult<()> {
+    // Extract Python list elements as Rust types (e.g., strings)
+    let packages_rust: Vec<String> = packages
+        .iter()
+        .map(|pkg| pkg.extract::<String>())
+        .collect::<Result<_, _>>()?;
+
+    println!("Prefix: {}", prefix);
+    println!("Packages: {:?}", packages_rust);
+
+    // Call your internal logic with Rust types
+    // _install_pypi(prefix, packages_rust);
+    Ok(())
+}
+
+fn _install_pypi(prefix: &Path, packages: Vec<LockedPackage>) -> i32 {
+
+    let pkgs: Vec<&str> = packages.iter().map(|pkg| pkg.name()).collect();
+    0
 }
 
 /// A Python module implemented in Rust.
-#[pymodule]
+#[pymodule(name="_dof")]
 fn foo(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(install_pypi, m)?)?;
     Ok(())
 }
+
+
+// // Construct an update context and perform the actual update.
+// let lock_file_derived_data = UpdateContext::builder(self)
+//     .with_package_cache(package_cache)
+//     .with_no_install(options.no_install)
+//     .with_outdated_environments(outdated)
+//     .with_lock_file(lock_file)
+//     .with_glob_hash_cache(glob_hash_cache)
+//     .finish()
+//     .await?
+//     .update()
+//     .await?;
