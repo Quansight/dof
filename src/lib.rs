afbd21abac0291a94646936ea1a27b17904bff4f
pdmurray <peynmurray@gmail.com>
2025-02-17 14:05:26 -0800

    Continue plugging away

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,30 @@ use pyo3::prelude::*;
 use rattler_lock::LockedPackage;
 
 
+// In python:
+//
+// LockFile:
+//   list[Environment]
+//
+// Environment:
+//   packages() -> list[LockedPackage]
+//
+// LockedPackage:
+//   _package: PyLockedPackage
+//
+// PyLockedPackage(LockedPackage):
+//   _package: PyLockedPackage  // <-- Must have .is_pypi = True for this to work
 
 #[pyfunction]
 fn install_pypi<'py>(py: Python<'py>, prefix: String, packages: &Bound<'py, PyList>) -> PyResult<()> {
-    // Extract Python list elements as Rust types (e.g., strings)
-    let packages_rust: Vec<String> = packages
+    let py_locked_packages: Vec<Bound<'py, PyAny>> = packages
         .iter()
-        .map(|pkg| pkg.extract::<String>())
-        .collect::<Result<_, _>>()?;
+        .map(|pkg| pkg.getattr("_package"))
+        .collect::<PyResult<Vec<_>>>()?;
 
     println!("Prefix: {}", prefix);
-    println!("Packages: {:?}", packages_rust);
+    println!("Packages: {:?}", py_locked_packages);
 
-    // Call your internal logic with Rust types
-    // _install_pypi(prefix, packages_rust);
     Ok(())
 }
 
@@ -25,6 +35,18 @@ fn _install_pypi(prefix: &Path, packages: Vec<LockedPackage>) -> i32 {
 
     let pkgs: Vec<&str> = packages.iter().map(|pkg| pkg.name()).collect();
     0
+}
+
+fn install_lockfile<'py>(py: Python<'py>, prefix: String, py_lockfile: &Bound<'py, PyAny>) -> PyResult<()> {
+    let py_env: Bound<'py, PyAny> = py_lockfile.call_method0("default_environment")?;
+    let py_platform: Bound<'py, PyAny> = PyListMethods::get_item(
+        py_env.call_method0("platforms")?.downcast()?,
+        0,
+    )?;
+
+    let packages: Bound<'py, PyAny> = py_env.call_method1("packages", py_platform)?;
+
+    Ok(())
 }
 
 /// A Python module implemented in Rust.
